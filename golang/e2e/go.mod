f686bd5484210a3546cc5462986299ed9c14beb4
satoya <mentol310@gmail.com>
2020-05-01 21:16:32 +0900

    add db test

--- golang/e2e/go.mod
+++ golang/e2e/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.4.1
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.7.4
 )
--- golang/e2e/main_test.go
+++ golang/e2e/main_test.go
@@ -8,16 +8,18 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 func TestStartServer(t *testing.T) {
-	want := "Hello, world!\n"
-
-	srv := buildServer(want)
+	srv := buildServer()
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal(err)
@@ -41,11 +43,64 @@ func TestStartServer(t *testing.T) {
 	}
 	res.Body.Close()
 
-	if string(b) != want {
-		t.Fatalf("want %q, but %q", want, b)
+	expect := "Hello, world!!"
+	if string(b) != expect {
+		t.Fatalf("want %q, but %q", expect, b)
 	}
 
 	if err := srv.Shutdown(context.Background()); err != nil {
+		t.Fatalf("HTTP server Shutdown: %v", err)
+	}
+
+	<-idleConnsClosed
+}
+
+func TestGetUserServer(t *testing.T) {
+	var res *http.Response
+	var err error
+
+	srv := buildServer()
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		if err = srv.Serve(l); err != http.ErrServerClosed {
+			t.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+		close(idleConnsClosed)
+	}()
+
+	retry := 5
+	for {
+		res, err = http.Get("http://" + l.Addr().String() + "/user")
+		if err == nil {
+			break
+		}
+		if retry == 0 {
+			break
+		}
+		retry--
+		time.Sleep(5 * time.Second)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res.Body.Close()
+
+	expect := "1:Olga:1978-01-06T15:27:33Z2:Scot:2002-07-19T04:24:42Z"
+	if string(b) != expect {
+		t.Fatalf("want %q, but %q", expect, b)
+	}
+
+	if err = srv.Shutdown(context.Background()); err != nil {
 		t.Fatalf("HTTP server Shutdown: %v", err)
 	}
 
@@ -76,13 +131,52 @@ func TestCreateItemWithDBMock(t *testing.T) {
 	}
 }
 
-func buildServer(body string) *http.Server {
+func dbConnect() (*sql.DB, error) {
+	source := os.Getenv("USER") + ":" + os.Getenv("PASSWORD") + "@tcp(" + os.Getenv("HOST") + ":" + os.Getenv("PORT") + ")/" + os.Getenv("DATABASE") + "?parseTime=true"
+	return sql.Open("mysql", source)
+}
+
+type User struct {
+	ID      int
+	Name    string
+	Created time.Time
+}
+
+func (u *User) ToString() string {
+	return strconv.Itoa(u.ID) + ":" + u.Name + ":" + u.Created.Format(time.RFC3339)
+}
+
+func buildServer() *http.Server {
 	r := mux.NewRouter()
 
 	r.Methods("GET").
-		PathPrefix("/").
+		Path("/").
 		HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			io.WriteString(w, body)
+			io.WriteString(w, "Hello, world!!")
+		})
+	r.Methods("GET").
+		Path("/user").
+		HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			db, err := dbConnect()
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+			}
+			defer db.Close()
+
+			rows, err := db.Query("select * from users;")
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+			}
+			defer rows.Close()
+
+			for rows.Next() {
+				var user User
+				err = rows.Scan(&user.ID, &user.Name, &user.Created)
+				if err != nil {
+					http.Error(w, err.Error(), 500)
+				}
+				io.WriteString(w, user.ToString())
+			}
 		})
 
 	return &http.Server{
